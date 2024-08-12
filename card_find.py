2c49677b0b2c319b46b939f50764578f7633d209
Michael Chhay <146492571+michaelchhay@users.noreply.github.com>
2024-08-12 11:16:43 -0700

    Create card_find.py

---
+++ card_find.py
@@ -0,0 +1,20 @@
+import pilxi
+
+IP_Address = "169.254.215.49"
+
+# Port, timeout parameters are optional, defaults will be used otherwise.
+session = pilxi.Pi_Session(IP_Address)
+# or 
+session = pilxi.Pi_Session(IP_Address, port=1024, timeout=5000)
+
+# Get list of Card IDs for cards in chassis
+cardID_array = session.GetUsableCards(0)
+print("Card IDs:", cardID_array)
+
+
+# Get list of Card Bus and Device Numbers
+card_array = session.FindFreeCards()
+
+for card in card_array:
+    bus, device = card
+    print("Card at bus {} device {}".format(bus, device))
